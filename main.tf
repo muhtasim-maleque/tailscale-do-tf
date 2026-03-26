e5db4cbd55e9e7047af4e07aebffbe4607fb2cf3
Muhtasim Maleque <niloy@Muhtasims-Mac-mini.local>
2026-03-26 11:16:39 -0400

    Remove digitalocean_vpc resource

--- main.tf
+++ main.tf
@@ -1,16 +1,9 @@
-resource "digitalocean_vpc" "interview_network" {
-  name     = "tailscale-interview-vpc"
-  region   = var.region
-  ip_range = "10.10.10.0/24"
-}
-
 resource "digitalocean_droplet" "tailscale" {
   image    = var.ubuntu_image
   name     = "tailscalesubnet"
   region   = var.region
   size     = var.droplet_size
   ssh_keys = [var.ssh_key_id]
-  vpc_uuid = digitalocean_vpc.interview_network.id
 
   user_data = templatefile("cloud_init.tftpl", {
     tailscale_auth_key = var.tailscale_auth_key
@@ -24,5 +17,4 @@ resource "digitalocean_droplet" "basic" {
   region   = var.region
   size     = var.droplet_size
   ssh_keys = [var.ssh_key_id]
-  vpc_uuid = digitalocean_vpc.interview_network.id
 }
